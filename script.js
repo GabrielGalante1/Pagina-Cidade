c890eba68108806947ac829db0e7fee37a6efd2d
Gabriel Galante <159261584+GabrielGalante1@users.noreply.github.com>
2024-07-31 09:37:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let imagens = document.getElementById('img1')
+let legendas = document.getElementById('legenda')
+function image1()
+{
+    imagens.src="Imagens/alb1.jpg"
+    legendas.innerHTML="Centro de Albuquerque"
+}
+function image2()
+{
+    imagens.src="Imagens/alb2.jpg"
+    legendas.innerHTML="Vista panorâmica do Centro de Albuquerque"
+}
+function image3()
+{
+    imagens.src="Imagens/alb3.jpeg"
+    legendas.innerHTML="Albuquerque de noite"
+}
+function image4()
+{
+    imagens.src="Imagens/heisenberg.jpg"
+    legendas.innerHTML="Heisenberg enterrando dinheiro no High Desert"
+}
